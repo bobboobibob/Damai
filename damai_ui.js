c52fa17fd8830f36caa3392ed3ffb111fcd27c37
bobboobibob <explorerb@outlook.com>
2025-04-12 19:10:53 +0800

    Update damai_ui.js

--- damai_ui.js
+++ damai_ui.js
@@ -4,25 +4,5 @@
     // 调试通知
     $notification.post("界面加载", "damai_ui.js 已加载", "如果看到此通知，说明脚本已运行");
 
-    // 简单界面
-    $ui.render({
-        props: {
-            title: "大麦抢票"
-        },
-        views: [
-            {
-                type: "label",
-                props: {
-                    text: "脚本已加载！",
-                    font: $font(16),
-                    textColor: $color("#000000"),
-                    align: $align.center
-                },
-                layout: function(make, view) {
-                    make.center.equalTo(view.super);
-                    make.width.equalTo(view.super);
-                }
-            }
-        ]
-    });
+    $done();
 })();
